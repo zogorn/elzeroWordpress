a9c9d715d92a187fba9db7ba79befa4987516d1f
zogorn <king.zogorn@gmail.com>
2017-07-06 07:22:40 +0200

    update syntax mistake

--- functions.php
+++ functions.php
@@ -7,11 +7,13 @@
 ** get_template_directory_uri();
 */
 
+// define styles
 function elzero_style() {
    wp_enqueue_style('font-awesome-style' , get_template_directory_uri() . '/assets/css/font-awesome.min.css', array(),'4.7.0');
    wp_enqueue_style('my-style' , get_template_directory_uri() . '/assets/css/style.min.css', array(),'1.0.0');
 }
 
+// define scripts
 function elzero_scripts() {
-   wp_enqueue_scripts('my-main-script', get_template_directory_uri() . 'assets/js/main.js', array(), '1.0.0', true);
+   wp_enqueue_script('my-main-script', get_template_directory_uri() . 'assets/js/main.js', array(), '1.0.0', true);
 }
