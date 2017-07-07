4a326ff61d7bb2931fadcac63f92e2bb7160c8d7
zogorn <king.zogorn@gmail.com>
2017-07-07 11:34:41 +0200

    add conditional html5shiv and respondjs in the header

--- functions.php
+++ functions.php
@@ -32,6 +32,12 @@ function elzero_scripts() {
    wp_enqueue_script('jquery');
    // add the theme js file to the front-end pages
    wp_enqueue_script('my-main-script', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0.0', true);
+   // add html5shiv to the header
+   wp_enqueue_script('html5shiv', get_template_directory_uri() . '/assets/js/html5shiv.js', array(), '3.7.3', false);
+   // make if condition to just import it if the browser was less than IE 9
+   wp_script_add_data('html5shiv', 'conditional', 'lt IE 9');
+   wp_enqueue_script('respond', get_template_directory_uri() . '/assets/js/respond.js', array(), '1.4.2', false);
+   wp_script_add_data('respond', 'conditional', 'lt IE 9');
 }
 
 
