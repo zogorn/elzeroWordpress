7018f36e0b20770378a26b5417a14e86c118811d
zogorn <king.zogorn@gmail.com>
2017-08-04 11:14:24 +0200

    style blog posts

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-<footer class="footer">
+<!-- <footer class="footer">
    Copyright (c) 2017 <a href="http://www.ahmedzaid.com">Ahmed Zaid</a> All Rights Reserved.
-</footer>
+</footer> -->
 <?php wp_footer(); ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,127 @@
 <?php get_header(); ?>
 
+<div class="container">
+   <div class="row">
+
+      <div class="col-md-6">
+         <div class="blog-post">
+            <div class="heading text-capitalize">
+               <h3 class="h1 text-capitalize"><a href="#">the post title</a></h3>
+               <span><i class="fa fa-user fa-fw text-muted" aria-hidden="true"></i><a href="#">elzero</a></span>
+               <span><i class="fa fa-calendar fa-fw text-muted" aria-hidden="true"></i><a href="#">20/07/2018</a></span>
+               <span><i class="fa fa-comments fa-fw text-muted" aria-hidden="true"></i><a href="#">17 comments</a></span>
+            </div><!-- heading -->
+            <div class="image">
+               <a href="#"><img src="http://via.placeholder.com/350" alt="" class="image-responsive"></a>
+            </div><!-- image -->
+            <div class="content text-justify">
+               <p class="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div><!-- content -->
+            <div class="tags text-uppercase">
+               <i class="fa fa-tags fa-fw text-muted" aria-hidden="true"></i>
+               <span><a href="#">item 1</a></span>
+               <span><a href="#">item 2</a></span>
+               <span><a href="#">item 3</a></span>
+               <span><a href="#">item 4</a></span>
+            </div><!-- tags -->
+         </div><!-- blog-post -->
+      </div><!-- col-md-6 -->
+      <div class="col-md-6">
+         <div class="blog-post">
+            <div class="heading">
+               <h3 class="h1 text-capitalize"><a href="#">the post title</a></h3>
+               <span><a href="#">elzero</a></span>
+               <span><a href="#">20/07/2018</a></span>
+               <span><a href="#">17 comments</a></span>
+            </div><!-- heading -->
+            <div class="image">
+               <a href="#"><img src="http://via.placeholder.com/350" alt="" class="image-responsive"></a>
+            </div><!-- image -->
+            <div class="content text-justify">
+               <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div><!-- content -->
+            <div class="tags">
+               <span><a href="#">item 1</a></span>
+               <span><a href="#">item 2</a></span>
+               <span><a href="#">item 3</a></span>
+               <span><a href="#">item 4</a></span>
+            </div><!-- tags -->
+         </div><!-- blog-post -->
+      </div><!-- col-md-6 -->
+      <div class="col-md-6">
+         <div class="blog-post">
+            <div class="heading">
+               <h3 class="h1 text-capitalize"><a href="#">the post title</a></h3>
+               <span><a href="#">elzero</a></span>
+               <span><a href="#">20/07/2018</a></span>
+               <span><a href="#">17 comments</a></span>
+            </div><!-- heading -->
+            <div class="image">
+               <a href="#"><img src="http://via.placeholder.com/350" alt="" class="image-responsive"></a>
+            </div><!-- image -->
+            <div class="content text-justify">
+               <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div><!-- content -->
+            <div class="tags">
+               <span><a href="#">item 1</a></span>
+               <span><a href="#">item 2</a></span>
+               <span><a href="#">item 3</a></span>
+               <span><a href="#">item 4</a></span>
+            </div><!-- tags -->
+         </div><!-- blog-post -->
+      </div><!-- col-md-6 -->
+      <div class="col-md-6">
+         <div class="blog-post">
+            <div class="heading">
+               <h3 class="h1 text-capitalize"><a href="#">the post title</a></h3>
+               <span><a href="#">elzero</a></span>
+               <span><a href="#">20/07/2018</a></span>
+               <span><a href="#">17 comments</a></span>
+            </div><!-- heading -->
+            <div class="image">
+               <a href="#"><img src="http://via.placeholder.com/350" alt="" class="image-responsive"></a>
+            </div><!-- image -->
+            <div class="content text-justify">
+               <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div><!-- content -->
+            <div class="tags">
+               <span><a href="#">item 1</a></span>
+               <span><a href="#">item 2</a></span>
+               <span><a href="#">item 3</a></span>
+               <span><a href="#">item 4</a></span>
+            </div><!-- tags -->
+         </div><!-- blog-post -->
+      </div><!-- col-md-6 -->
+      <div class="col-md-6">
+         <div class="blog-post">
+            <div class="heading">
+               <h3 class="h1 text-capitalize"><a href="#">the post title</a></h3>
+               <span><a href="#">elzero</a></span>
+               <span><a href="#">20/07/2018</a></span>
+               <span><a href="#">17 comments</a></span>
+            </div><!-- heading -->
+            <div class="image">
+               <a href="#"><img src="http://via.placeholder.com/350" alt="" class="image-responsive"></a>
+            </div><!-- image -->
+            <div class="content text-justify">
+               <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div><!-- content -->
+            <div class="tags">
+               <span><a href="#">item 1</a></span>
+               <span><a href="#">item 2</a></span>
+               <span><a href="#">item 3</a></span>
+               <span><a href="#">item 4</a></span>
+            </div><!-- tags -->
+         </div><!-- blog-post -->
+      </div><!-- col-md-6 -->
+   </div><!-- row -->
+</div><!-- container -->
+
+
+
+
+
+
+
+
 <?php get_footer(); ?>
