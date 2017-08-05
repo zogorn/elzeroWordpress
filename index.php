b37c675690f3af8806f9fc738b2fe6c7898b3946
zogorn <king.zogorn@gmail.com>
2017-08-05 11:33:45 +0200

    add post thumbnail, author, date with dynamic content

--- index.php
+++ index.php
@@ -2,34 +2,45 @@
 
 <div class="container">
    <div class="row">
-      <?php if (have_posts()) {// check for posts
-         while (have_posts()) {// loop to get all posts
+      <?php if (have_posts()) :// check for posts
+         while (have_posts()) :// loop to get all posts
             the_post(); ?>
             <div class="col-md-6">
                <div class="blog-post">
                   <div class="heading text-capitalize">
-                     <?php the_title('<h3 class="h1 text-capitalize"><a href="#">','</a></h3>') ?>
-                     <span><i class="fa fa-user fa-fw text-muted" aria-hidden="true"></i><a href="#">elzero</a></span>
-                     <span><i class="fa fa-calendar fa-fw text-muted" aria-hidden="true"></i><a href="#">20/07/2018</a></span>
-                     <span><i class="fa fa-comments fa-fw text-muted" aria-hidden="true"></i><a href="#">17 comments</a></span>
+                     <h3 class="h1 text-capitalize">
+                        <!-- link for the post title -->
+                        <a href="<?php the_permalink() ?>">
+                           <?php the_title(); ?>
+                        </a>
+                     </h3>
+                     <span><i class="fa fa-user fa-fw text-muted" aria-hidden="true"></i>
+                        <?php the_author_posts_link() ?>
+                     </span>
+                     <span><i class="fa fa-calendar fa-fw text-muted" aria-hidden="true"></i>
+                        <?php the_time('F, j, M') ?>
+                     </span>
+
+                     <span><i class="fa fa-comments fa-fw text-muted" aria-hidden="true"></i>
+                        <?php comments_popup_link('0 comments', '1 comment', '% comments', 'comments desabled') ?>
+                     </span>
                   </div><!-- heading -->
                   <div class="image">
-                     <a href="#"><img src="http://via.placeholder.com/350" alt="" class="image-responsive"></a>
+                     <a href="<?php the_permalink() ?>">
+                        <?php the_post_thumbnail('', ['class' => 'image-responsive img-thumbnail']) ?>
+                     </a>
                   </div><!-- image -->
                   <div class="content text-justify">
                      <p class="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                   </div><!-- content -->
                   <div class="tags text-uppercase">
                      <i class="fa fa-tags fa-fw text-muted" aria-hidden="true"></i>
-                     <span><a href="#">item 1</a></span>
-                     <span><a href="#">item 2</a></span>
-                     <span><a href="#">item 3</a></span>
-                     <span><a href="#">item 4</a></span>
+                     <span><?php the_category(' , ') ?></span>
                   </div><!-- tags -->
                </div><!-- blog-post -->
             </div><!-- col-md-6 -->
-      <?php }// end while
-      }// end if ?>
+   <?php endwhile;
+   endif; ?>
 
 
    </div><!-- row -->
