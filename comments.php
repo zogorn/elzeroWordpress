d6c198b58b2362bd59535d7bd2ef1052e37ac49b
zogorn <king.zogorn@gmail.com>
2017-08-22 13:19:00 +0200

    add some comments functions

--- comments.php
+++ comments.php
@@ -1,1 +1,14 @@
 <?php
+
+if (comments_open()) {
+   $comments_props = array(
+      'max_depth'   => 3,
+      'type'        => 'comment',
+      'avatar_size' => 64
+   );
+   echo "<ul class = 'comments-list list-unstyled'>";
+      wp_list_comments($comments_props);
+   echo "</ul>";
+} else {
+   echo "Comments Are Disabled";
+}
